07cfbb2f4db77cf6c3ebda2f5a2cb535a443a41a
Rupesh sah <rsah0123456@gmail.com>
2025-09-29 09:14:46 +0000

    High-level problems (what’s breaking / likely to break)
    Based on the re

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,
+  },
   images: {
     remotePatterns: [
       {
