f747101af19ed7ffc13b186c6f7089c604cc64c5
rupesh-kumar-sah <rsah0123456@gmail.com>
2025-10-03 10:00:17 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/studio',
-    assetPrefix: '/studio',
     trailingSlash: true,
     images: {
         unoptimized: true,
